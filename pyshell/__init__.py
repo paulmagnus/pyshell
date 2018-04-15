6145f6a99d284ff9a9e5b60c7eb367dfc2fb8bd8
paulmagnus <pmagnusnh@gmail.com>
2018-04-14 22:18:45 -0400

    Simplified Stream and Process classes so that the process tree can be built in any order. Translation is not updated for this update yet. Use of try except for creation of streams is needed often if translation is to be direct and straightforward.

--- pyshell/__init__.py
+++ pyshell/__init__.py
@@ -1,4 +1,4 @@
 # Make the process file available from the module level
 from pyshell.process import *
-from pyshell.stream import *
+# from pyshell.stream import *
 #from pyshell.process import PIPE, DEVNULL, STDOUT
